55259f5d0d86004059a5909a44cbb87fef7f84c2
soitknows <and.tarver@gmail.com>
2017-10-25 21:28:41 -0700

    added Shiny app functionality, with placholder plots

--- app.R
+++ app.R
@@ -1,4 +1,5 @@
 library(rjson)
+library(shiny)
 
 GetPatches <- function (){
   vers_url <- "https://ddragon.leagueoflegends.com/api/versions.json"
@@ -6,14 +7,61 @@ GetPatches <- function (){
   versions <- versions[!grepl("lolpatch", versions)]
 }
 
-GetPatchStats <- function (patch) {
+GetPatchData <- function (patch) {
     url_beg <- "http://ddragon.leagueoflegends.com/cdn/"
     url_end <- "/data/en_US/champion.json"
     url <- paste(url_beg, patch, url_end, sep = "")
-    stats <- fromJSON(file = url)
+    data <- fromJSON(file = url)
+    data <- data[['data']]
 }
 
-GetChampStats <- function(stats,champ) {
-  stats <- data[['data']]
-  champ_stats <- stats[champ]
+GetChamps <- function (data) {
+  champs <- names(data)
 }
+  
+GetChampStats <- function (data,champ) {
+  champ_stats <- data[[champ]][['stats']]
+}
+
+patches <- GetPatches()
+data <- GetPatchData(patches[1])
+champs <- GetChamps(data)
+
+
+
+ui <- fluidPage(
+  selectInput(inputId = "patch",
+              label = "Patch",
+              choices = patches),
+  
+  selectInput(inputId = "champ1",
+              label = "Champion 1",
+              choices = champs),
+  
+  selectInput(inputId = "champ2",
+              label = "Champion 2",
+              choices = champs),
+  
+  plotOutput(outputId = "stats1"),
+  plotOutput(outputId = "stats2")
+)
+
+server <- function (input, output) {
+  df1 <- reactive({
+    stats <- GetChampStats(data, input$champ1)
+    stats <- unlist(stats)
+  })
+  df2 <- reactive({
+    stats <- GetChampStats(data, input$champ2)
+    stats <- unlist(stats)
+  })
+  
+  output$stats1 <- renderPlot({
+    plot(df1())
+  })
+  output$stats2 <- renderPlot({
+    plot(df2())
+  })
+}
+
+shinyApp(ui = ui, server = server)
