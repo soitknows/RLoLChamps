5253d5ca594e4392e65cdd6eea05d80d35e18098
soitknows <and.tarver@gmail.com>
2017-10-25 19:37:40 -0700

    added GetPatches, GetPatchStats, GetChampStats functions

--- app.R
+++ app.R
@@ -1,13 +1,19 @@
 library(rjson)
 
-vers_url <- "https://ddragon.leagueoflegends.com/api/versions.json"
-versions <- fromJSON(vers_url)
-versions <- versions[!grepl("lolpatch", versions)]
+GetPatches <- function (){
+  vers_url <- "https://ddragon.leagueoflegends.com/api/versions.json"
+  versions <- fromJSON(file = vers_url)
+  versions <- versions[!grepl("lolpatch", versions)]
+}
 
-
-GetChampStats <- function (patch) {
+GetPatchStats <- function (patch) {
     url_beg <- "http://ddragon.leagueoflegends.com/cdn/"
     url_end <- "/data/en_US/champion.json"
     url <- paste(url_beg, patch, url_end, sep = "")
-    stats <- fromJSON(url)
+    stats <- fromJSON(file = url)
+}
+
+GetChampStats <- function(stats,champ) {
+  stats <- data[['data']]
+  champ_stats <- stats[champ]
 }
